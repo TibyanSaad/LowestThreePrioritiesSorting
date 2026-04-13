ce96c1f13ba2fb9cc837146f6f77b548ecbfa9e5
TibyanSaad <saad.tibyan02@gmail.com>
2026-04-13 16:38:39 +0400

    final prject structure

--- src/ComplaintService.java
+++ src/ComplaintService.java
@@ -1,2 +1,12 @@
+import java.util.Arrays;
+
 public class ComplaintService {
+    public int[] getLowestThree(int[] priorities, SortStrategy strategy) {
+
+        if (priorities == null || priorities.length < 3) {
+            throw new IllegalArgumentException("There must be at least 3 priorities");
+        }
+        strategy.sort(priorities);
+        return Arrays.copyOf(priorities, 3);
+    }
 }
--- src/InputParser.java
+++ src/InputParser.java
@@ -1,2 +1,15 @@
 public class InputParser {
+    public static int[] parse(String input) {
+        if (!input.contains(",")) {
+            System.err.println("Invalid input format. Please enter integers separated by commas.");
+            return null;
+        } else {
+            String[] parts = input.split(",");
+            int[] priorities = new int[parts.length];
+            for (int i = 0; i < parts.length; i++) {
+                priorities[i] = Integer.parseInt(parts[i].trim());// convert to array
+            }
+            return priorities;
+        }
+    }
 }
